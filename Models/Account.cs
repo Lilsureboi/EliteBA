277a88d5309675b5c9ba5b666acaa00e2fa31e95
Claudius A. <ayadiclaudius@gmail.com>
2025-04-12 13:25:42 +0100

    fix: refactored the Balance property to reflect the default.
    Signed-off-by: Claudius A. <ayadiclaudius@gmail.com>

--- Models/Account.cs
+++ Models/Account.cs
@@ -2,19 +2,11 @@
 
 public class Account
 {
-    private double _balance = 0;
-
-    public int AccountId { get; set; } 
+    public int AccountId { get; set; }
     public required string AccountNumber { get; set; }
     public required string AccountName { get; set; }
     public AccountTypeEnum AccountType { get; set; }
-
-    public double Balance
-    {
-        get => _balance;
-        set => _balance = value;
-    }
-
+    public double Balance { get; set; } = 0.00;
     public DateTime DateOpened { get; set; }
     public AccountStatusEnum AccountStatus { get; set; }
     public List<Transaction> Transactions { get; set; } = [];
