b789b2d29d2c886ee39373d42f6a4c6b8c72104e
Fortunate Omonuwa <omonuwafortunate@gmail.com>
2025-04-12 19:20:36 +0100

    update some properties in the models

--- Models/Transaction.cs
+++ Models/Transaction.cs
@@ -4,24 +4,15 @@ public class Transaction
 {
     public int TransactionId { get; set; }
     public int AccountId { get; set; }
-    public TransactionTypeEnum TransactionType { get; set; }
+    public TransactionType TransactionType { get; set; }
     public double Amount { get; set; }
     public string? Narration { get; set; }
     public DateTime DateCreated { get; set; }
-
-    public int
-        RecipientAccountId
-    {
-        get;
-        set;
-    } // For transfers (not applicable to deposit and withdrawal) - @EJ, please read up the tutor's review on this. Consult him for proper usage.
-
-    public decimal
-        BalanceAfter
-    { get; set; } // helper property - e.g. BalanceAfter = Account.Balance + transaction.Amount
+    public int RecipientAccountId { get; set;} // For transfers (not applicable to deposit and withdrawal) - @EJ, please read up the tutor's review on this. Consult him for proper usage.
+    public decimal BalanceAfter { get; set; } // helper property - e.g. BalanceAfter = Account.Balance + transaction.Amount
 }
 
-public enum TransactionTypeEnum
+public enum TransactionType
 {
     Deposit,
     Withdrawal,
