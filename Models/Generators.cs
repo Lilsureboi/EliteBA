b6eae700c8a26c392d0fa8755df959724e03f098
Claudius A. <90831377+claudiusayadi@users.noreply.github.com>
2025-04-12 12:19:12 +0100

    chore: added suggested comments to Models/Generators methods
    Co-authored-by: Fortunate Omonuwa <omonuwafortunate@gmail.com>

--- Models/Generators.cs
+++ Models/Generators.cs
@@ -7,6 +7,7 @@ public class Generators
     private static int _transactionIdCounter;
     private static readonly Random _random = new Random();
 
+//This method handles the generation of account Ids. This is going to be our unique identifier for each account created. The method increments the accountId by 1 for each account created
     public static int GenerateAccountId() => _accountIdCounter++;
     public static int GenerateCustomerId() => _customerIdCounter++;
     public static int GenerateTransactionId() => _transactionIdCounter++;
