787fcb4848800ca7bd379573b1792f491832ae43
Claudius A. <ayadiclaudius@gmail.com>
2025-04-14 15:18:47 +0100

    fix: refactor RecipientAccountId property for transfer validation and update BankOperations class structure

--- EliteBA.Models/Transaction.cs
+++ EliteBA.Models/Transaction.cs
@@ -8,8 +8,21 @@ public class Transaction
     public double Amount { get; set; }
     public string? Narration { get; set; }
     public DateTime DateCreated { get; set; }
-    public int RecipientAccountId { get; set;} // For transfers (not applicable to deposit and withdrawal) - @EJ, please read up the tutor's review on this. Consult him for proper usage.
+    public bool IsTransfer;
 
+    private int _recipientAccountId;
+    public int RecipientAccountId
+    {
+        get => _recipientAccountId;
+        set
+        {
+            do
+            {
+                Console.WriteLine("Recipient account ID must be greater than 0 for transfers");
+            } while (IsTransfer && value <= 0);
+            _recipientAccountId = value;
+        }
+    }
 }
 
 public enum TransactionType
--- EliteBA.Operations/BankOperations.cs
+++ EliteBA.Operations/BankOperations.cs
@@ -1,6 +1,8 @@
+using EliteBA.Models;
+
 namespace EliteBA.Operations;
 
 public class BankOperations
 {
-    
+    private static readonly List<Transaction> _transactions = new List<Transaction>();
 }
